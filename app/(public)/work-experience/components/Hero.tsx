30023457564c0bfd4c7e898f8aae54b757ac8f42
rehanardianshahab <rehanardian1@gmail.com>
2025-05-12 15:47:10 +0700

    add: work experience page

---
+++ app/(public)/work-experience/components/Hero.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import AnimatedContent from '@/components/animations/AnimatedContent/AnimatedContent';
+import SplitText from '@/components/animations/SplitText/SplitText';
+import { useState } from 'react';
+
+export default function Hero() {
+  const [isAnimation, setIsAnimation] = useState<Boolean>(false);
+
+  const handleAnimationComplete = () => {
+    setIsAnimation(true);
+  };
+
+  return (
+    <div className="flex h-[calc(90vh)] items-center justify-center text-center">
+      <div className="space-y-10">
+        <div>
+          <SplitText
+            text="Career Journey"
+            className="text-6xl font-semibold text-white"
+            delay={150}
+            animationFrom={{ opacity: 0, transform: 'translate3d(100px,0,0)' }}
+            animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+            threshold={0.2}
+            onLetterAnimationComplete={handleAnimationComplete}
+          />
+        </div>
+        {!isAnimation ? (
+          <div className="h-[calc(24vh-10px)]">&nbsp;</div>
+        ) : (
+          <div className="h-[calc(24vh-10px)]">
+            <AnimatedContent
+              distance={0}
+              direction="vertical"
+              reverse={false}
+              delay={400}
+              config={{ tension: 40, friction: 30 }}
+              initialOpacity={0.0}
+              animateOpacity
+              scale={1.1}
+              threshold={0.1}
+            >
+              <div className="space-y-10">
+                <div>
+                  <p className="text-lg text-white">
+                    This section showcases my career path as a Frontend Developer, highlighting my
+                    experience, growth,
+                  </p>
+                  <p className="text-lg text-white">
+                    and contributions in building engaging digital experiences.
+                  </p>
+                </div>
+              </div>
+            </AnimatedContent>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- app/(public)/work-experience/page.tsx
+++ app/(public)/work-experience/page.tsx
@@ -1,3 +1,20 @@
+import Footer from '@/app/components/Footer';
+import type { Metadata } from 'next';
+import Hero from './components/Hero';
+
+export const metadata: Metadata = {
+  title: 'Portfolio - Rehan Ardian',
+  description: 'Hi, I&apos;m Rehan'
+};
+
 export default function Page() {
-  return <>Hello</>;
+  return (
+    <>
+      <div className="space-y-42 px-4 lg:space-y-30 lg:px-0">
+        <Hero />
+        <div className="space-y-18 lg:space-y-12">Hello</div>
+      </div>
+      <Footer />
+    </>
+  );
 }
